a87cf1bf5da4e4bcf84f680d28035793c389d608
melissataan <melissataan@ymail.com>
2020-06-15 22:26:32 +0800

    Initial commit
    Description:
    -- Draft
    -- Parsing of inputs
    -- Data validation
    -- Struct as key in map

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <map>
+
+// #include "input_parser.hpp"
+
+// struct SkiNode
+// {
+// };
+
+// struct SkiRelation
+// {
+// };
+
+struct KeyDimension
+{
+    KeyDimension(int row, int col)
+    {
+        this->row = row;
+        this->col = col;
+    }
+
+    bool operator=(const KeyDimension& key)
+    {
+        return row == key.row && col == key.col;
+    }
+
+    bool operator<(const KeyDimension& key) const {
+         return (row < key.row  || (row == key.row  && col < key.col ));
+    }
+
+    int row = 0;
+    int col = 0;
+};
+
+std::vector<int> findLongestSki(const std::vector<std::vector<int>>& input)
+{
+    std::vector<int> result;
+
+    std::map<KeyDimension, int> keyMap;
+
+    int rowLength = input[0][0];
+    int colLength = input[0][1];
+
+    std::vector<std::vector<int>> inputCopy(input.begin() + 1, input.end());
+
+    // print input data
+    // for (auto i : inputCopy)
+    // {
+    //     for (auto j : i)
+    //         LOG(("%u ", j));
+    //     LOG(("\n"));
+    // }
+
+    for (auto row = 0; row < rowLength; ++row)
+    {
+        for (auto col = 0; col < colLength; ++col)
+        {
+            KeyDimension key(row,col);
+            keyMap[key] = inputCopy[row][col];
+        }
+    }
+
+    // Print row, col, and value
+    // for (auto i : keyMap)
+    // {
+    //     LOG(("Row[%u] Col[%u]: Value[%u]\n\n", i.first.row, i.first.col, i.second));
+    // }
+
+    return result;
+}
+
+int main()
+{
+    std::vector<std::vector<int>> vectorInput = utils::readInput();
+    int rowSize = 0;
+    int colSize = 0;
+
+    if (vectorInput.size() == 0 || vectorInput[0].size() < 2)
+    {
+        LOG(("Incorrect input."));
+    }
+
+    rowSize = vectorInput[0][0];
+    colSize = vectorInput[0][1];
+
+    LOG(("Dimension is : %ux%u\n", rowSize, colSize));
+    LOG(("PRINT\n\n"));
+
+    findLongestSki(vectorInput);
+
+    return 0;
+}
